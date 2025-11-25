940b06d496f6f2786c5a9d3473fb428bdb785013
Alex <luisrodriguezluna32@gmail.com>
2025-11-24 22:41:50 -0600

    Agregue la configuracion del CORS

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from app.api import cuartos_api, huespedes_api, reservaciones_api
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Hotel API")
 app.include_router(cuartos_api.router)
 app.include_router(huespedes_api.router)
 app.include_router(reservaciones_api.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],  
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, PUT, DELETE)
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
 
 # Endpoint raiz
 @app.get("/")
